ee27e9b9375bc41f080d1fbfedf2798a9fae8369
sssv587 <1092663295@qq.com>
2024-06-10 16:17:32 +0800

    依赖范围之Compile

---
+++ maven-web/src/main/java/com/futurebytedance/maven/pojo/Student.java
@@ -0,0 +1,11 @@
+package com.futurebytedance.maven.pojo;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2024/6/10 - 16:11
+ * @Description
+ */
+
+public class Student {
+}
---
+++ maven-web/src/test/java/com/futurebytedance/maven/pojo/DruidTest.java
@@ -0,0 +1,17 @@
+package com.futurebytedance.maven.pojo;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2024/6/10 - 16:12
+ * @Description
+ */
+
+public class DruidTest {
+    @Test
+    public void testDruid(){
+
+    }
+}
