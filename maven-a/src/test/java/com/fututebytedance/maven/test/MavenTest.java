04d6c5135b4da6dc3e7756a78bdf3c094a6a555a
sssv587 <1092663295@qq.com>
2024-06-10 18:08:05 +0800

    Maven工程依赖得传递特性

---
+++ maven-a/src/test/java/com/fututebytedance/maven/test/MavenTest.java
@@ -0,0 +1,17 @@
+package com.fututebytedance.maven.test;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author yuhang.sun
+ * @version 1.0
+ * @date 2024/6/10 - 18:06
+ * @Description
+ */
+
+public class MavenTest {
+    @Test
+    public void testDruid() {
+
+    }
+}
